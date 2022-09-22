48ee3337f45e5d9163b26024f22cbf512b156b67
kabaodao <kabaodao@gmail.com>
2022-09-23 06:41:21 +0900

    [Updata/Edit] For discord.js v13 to v14. (delete some moduels)

--- src/commands/vroll.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-
-module.exports = {
-  data: new SlashCommandBuilder().setName('vroll').setDescription('divide'),
-  async execute(interaction) {
-    await interaction.reply('Pong!');
-  },
-};
--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,10 +1,14 @@
-const dotenv = require('dotenv');
 const fs = require('node:fs');
 const path = require('node:path');
 const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const { Routes } = require('discord.js');
+const dotenv = require('dotenv');
 
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, '../.env') });
+
+const clientId = process.env.DISCORD_CLIENT_ID;
+const guildId = process.env.DISCORD_GUILD_ID;
+const token = process.env.DISCORD_TOKEN;
 
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
@@ -18,15 +22,18 @@ for (const file of commandFiles) {
   commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
+const rest = new REST({ version: '10' }).setToken(token);
+
+// rest
+//   .delete(
+//     Routes.applicationGuildCommand(clientId, guildId, '984715369027608617'),
+//   )
+//   .then(() => console.log('Successfully deleted guild command'))
+//   .catch(console.error);
 
 rest
-  .put(
-    Routes.applicationGuildCommands(
-      process.env.DISCORD_CLIENT_ID,
-      process.env.DISCORD_GUILD_ID,
-    ),
-    { body: commands },
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then((data) =>
+    console.log(`Successfully registered ${data.length} application commands.`),
   )
-  .then(() => console.log('Successfuly registered application commands.'))
   .catch(console.error);
