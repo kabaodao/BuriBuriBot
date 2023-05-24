eaf1efdcf4030460355f7d98e1e62976e29dbb05
kabaodao <kabaodao@gmail.com>
2023-05-25 07:27:48 +0900

    Fix add role limit warning code

--- src/events/buttonInteraction.js
+++ src/events/buttonInteraction.js
@@ -5,24 +5,57 @@ module.exports = {
   name: Events.InteractionCreate,
   async execute(interaction) {
     if (!interaction.isButton()) return;
-    if (interaction.member.roles.cache.has('1105715047134675056')) return;
 
     const state = await svm.getInstanceState();
-    if (interaction.customId === 'start') {
-      if (state === 'stopped') {
-        try {
-          await svm.startInstance();
-          const embed = await svm.getEmbed('Server is starting!');
-          await interaction.update({
-            embeds: [embed],
-          });
-          await interaction.followUp(`<@${interaction.user.id}> has started the server!`);
-        } catch (e) {
-          console.log(e);
+
+    if (interaction.member.roles.cache.has('1105715047134675056')) {
+      if (interaction.customId === 'start') {
+        if (state === 'stopped') {
+          try {
+            await svm.startInstance();
+            const embed = await svm.getEmbed('Server is starting!');
+            await interaction.update({
+              embeds: [embed],
+            });
+            await interaction.followUp(`<@${interaction.user.id}> has started the server!`);
+          } catch (e) {
+            console.log(e);
+          }
+        } else {
+          try {
+            const embed = await svm.getEmbed(`Server is already ${state}!`);
+            await interaction.update({
+              embeds: [embed],
+            });
+          } catch (e) {
+            console.log(e);
+          }
         }
-      } else {
+      } else if (interaction.customId === 'stop') {
+        if (state === 'running') {
+          try {
+            await svm.stopInstance();
+            const embed = await svm.getEmbed('Server is stopping!');
+            await interaction.update({
+              embeds: [embed],
+            });
+            await interaction.followUp(`<@${interaction.user.id}> has stopped the server!`);
+          } catch (e) {
+            console.log(e);
+          }
+        } else {
+          try {
+            const embed = await svm.getEmbed(`Server is already ${state}!`);
+            await interaction.update({
+              embeds: [embed],
+            });
+          } catch (e) {
+            console.log(e);
+          }
+        }
+      } else if (interaction.customId === 'info') {
         try {
-          const embed = await svm.getEmbed(`Server is already ${state}!`);
+          const embed = await svm.getEmbed('Reload embed!');
           await interaction.update({
             embeds: [embed],
           });
@@ -30,37 +63,8 @@ module.exports = {
           console.log(e);
         }
       }
-    } else if (interaction.customId === 'stop') {
-      if (state === 'running') {
-        try {
-          await svm.stopInstance();
-          const embed = await svm.getEmbed('Server is stopping!');
-          await interaction.update({
-            embeds: [embed],
-          });
-          await interaction.followUp(`<@${interaction.user.id}> has stopped the server!`);
-        } catch (e) {
-          console.log(e);
-        }
-      } else {
-        try {
-          const embed = await svm.getEmbed(`Server is already ${state}!`);
-          await interaction.update({
-            embeds: [embed],
-          });
-        } catch (e) {
-          console.log(e);
-        }
-      }
-    } else if (interaction.customId === 'info') {
-      try {
-        const embed = await svm.getEmbed('Reload embed!');
-        await interaction.update({
-          embeds: [embed],
-        });
-      } catch (e) {
-        console.log(e);
-      }
+    } else {
+      await interaction.reply('You can not use this command.');
     }
   },
 };
